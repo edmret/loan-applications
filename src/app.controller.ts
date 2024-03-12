01f555bb3b6e936e69b3bd424a50736824c14939
Edmundo Retama <edmret@gmail.com>
2024-03-12 15:43:47 -0600

    implemented swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Request,
+  Post,
+  UseGuards,
+  HttpStatus,
+} from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { AppService } from './app.service';
 import { Public } from './decorators/public.decorator';
 import { Roles } from './decorators/roles.decorator';
 import { Role } from './enums/role.enum';
+import { CredentialsDto } from './dto/credentials.dto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { LoginDto } from './dto/login.dto';
 
+@ApiTags('Auth')
 @Controller()
 export class AppController {
   constructor(
@@ -13,10 +24,23 @@ export class AppController {
     private authService: AuthService,
   ) {}
 
+  @ApiOperation({
+    summary: 'Login',
+    description: 'Request a new token based on user credentials',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The login is success',
+    type: CredentialsDto,
+  })
+  @ApiBody({
+    type: LoginDto,
+    description: 'The JSON body to create a new item',
+  })
   @Public()
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req) {
+  async login(@Request() req): Promise<CredentialsDto> {
     return this.authService.login(req.user);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Enhanced Loan Application Processing Service Challenge')
+    .setDescription(
+      'microservice that manages loan applications, including submission, status checks, and admin management. Optionally, add authentication and authorization to secure the service and manage user roles.',
+    )
+    .setVersion('1.0')
+    .addTag('loans')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
